13d8c358ae172739f992c8b169a5a8c5890a3b85
TheDutchMC <github@thedutchmc.nl>
2021-02-02 14:31:16 +0000

    Automatic build for GH-Pages
    Signed-off-by: TheDutchMC <github@thedutchmc.nl>

--- src/ts/config.ts
+++
@@ -1,3 +0,0 @@
-//NO TRAILING SLASH!
-export const MUCE_API = "https://api.thedutchmc.nl/muce";
-export const MUCE_WORKSPACE = "https://workspace.muce.apps.thedutchmc.nl";
--- src/ts/index.ts
+++
@@ -1,8 +0,0 @@
-export { setupLoginForm } from "./loginForm";
-export { setupRegisterForm } from "./registerForm";
-export { loadWorkspace } from "./workspace";
-export { prepareContainer } from "./prepareContainer";
-export { checkHealth } from "./checkHealth";
-
-import "../scss/master.scss";
-
